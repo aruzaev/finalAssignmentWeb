6f660aeece5f23783e3aa68c9385f558f0cf62de
aruzaev <ruzaev.artem21@gmail.com>
2023-04-18 18:32:12 -0400

    gngnngnrfgsn;sfg

--- script/script.js
+++ script/script.js
@@ -1,9 +1,8 @@
 const planets = document.querySelectorAll('.planet');
 const planetNames = ['mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune', 'sun'];
 
-
 planets.forEach((planet, index) => {
   planet.addEventListener('click', () => {
-    window.location.href = `${planetNames[index]}.html`;
+    window.parent.location.href = `${planetNames[index]}.html`;
   });
 });

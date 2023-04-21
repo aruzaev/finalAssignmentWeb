916f6c17433ef9aa848975acc0fbc316aaf6eb9f
aruzaev <ruzaev.artem21@gmail.com>
2023-04-21 14:06:39 -0400

    gkpfngklfdnzgknoldfznlgldfkngnlfdk

--- test2/script.js
+++
@@ -1,7 +0,0 @@
-const hamburgerMenu = document.getElementById('hamburger-menu');
-const menuIcon = document.getElementById('menu-icon');
-const menu = document.getElementById('menu');
-
-menuIcon.addEventListener('click', () => {
-  hamburgerMenu.classList.toggle('open');
-});
